db78530616105e66495080b8dbe4359c5f138762
programmmer-dani <dani.jong01@gmail.com>
2025-06-16 19:54:14 +0200

    apps title changed

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'POC App',
+      title: 'final concept POC',
       home: LoginPage(),
     );
   }
